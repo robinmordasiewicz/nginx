32f57941754a64fb89e894e537aff6cc60b85d7d
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-07 19:18:14 -0400

    New pipeline test

--- increment-version.sh
+++ increment-version.sh
@@ -3,9 +3,16 @@
 
 set -e
 
-curl -s -L https://raw.githubusercontent.com/robinmordasiewicz/sphinx-build-container/main/VERSION --output VERSION.sphinx-build-container
+#curl -s -L https://raw.githubusercontent.com/robinmordasiewicz/sphinx-container/main/VERSION --output VERSION.sphinx-container
 
-SPHINXBUILDVERSION=`cat VERSION.sphinx-build-container | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
+[ -d tmp ] && rm -rf tmp
+mkdir tmp
+git clone https://github.com/robinmordasiewicz/sphinx-container.git tmp/
+#SPHINXBUILDVERSION=`cat tmp/VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
+SPHINXBUILDVERSION=`cat tmp/VERSION`
+cat tmp/VERSION
+[ -d tmp ] && rm -rf tmp
+echo $SPHINXBUILDVERSION
 
 cat Jenkinsfile | sed -re "s/^[[:space:]]*robinhoodis\/sphinx-build:.*/sphinx-build:${SPHINXBUILDVERSION}/" > Jenkinsfile.tmp && mv Jenkinsfile.tmp Jenkinsfile
 
