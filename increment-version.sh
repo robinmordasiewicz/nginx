b9e366bd0bae298a37b2a1c22d775e3c0ca8c942
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-11 18:55:56 -0400

    remove version file

--- increment-version.sh
+++ increment-version.sh
@@ -11,7 +11,8 @@ SPHINXBUILDVERSION=`cat tmp/VERSION`
 
 cat Jenkinsfile | sed -re "s/image:[[:space:]]robinhoodis\/sphinx:.*/image: robinhoodis\/sphinx:${SPHINXBUILDVERSION}/" > Jenkinsfile.tmp && mv Jenkinsfile.tmp Jenkinsfile
 
-LOCALREVISION=`cat VERSION | sed -re "s/^[0-9]+\.[0-9]+\.[0-9]+-*([0-9]*)/\1/" | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
+#LOCALREVISION=`cat VERSION | sed -re "s/^[0-9]+\.[0-9]+\.[0-9]+-*([0-9]*)/\1/" | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
 
-echo "${SPHINXBUILDVERSION}-${LOCALREVISION}" > VERSION
+#echo "${SPHINXBUILDVERSION}-${LOCALREVISION}" > VERSION
+#echo "${SPHINXBUILDVERSION}" > VERSION
 
