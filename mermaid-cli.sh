b22e84a7570a69538101e4d0e965cf4cad6564d6
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-05-29 21:43:13 -0400

    updating intro

--- mermaid-cli.sh
+++ mermaid-cli.sh
@@ -5,5 +5,5 @@ if [ -f diagram2.png ];then
   rm diagram2.png
 fi
 
-/usr/local/bin/mmdc -p /puppeteer-config.json -i docs/intro.mmd -o diagram2.png -C mermaid.css -c mermaid-config.json -t neutral
+/usr/local/bin/mmdc -p /puppeteer-config.json -i docs/intro.mmd -o diagram2.png -C sphinx-theme/mermaid.css -c sphinx-theme/mermaid-config.json -t neutral
 
