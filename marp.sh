e1ca88a5e0ff7cbc0aad107656f246e5c1b18a4e
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-05-30 13:15:59 -0400

    updating intro

--- marp.sh
+++ marp.sh
@@ -2,8 +2,8 @@
 #
 
 if test -f "/home/ubuntu/.cli/docker-entrypoint"; then
-    /home/ubuntu/.cli/docker-entrypoint docs/intro.md --theme spinx-theme/marp-theme.css --allow-local-files --pptx
+    /home/ubuntu/.cli/docker-entrypoint docs/intro.md --theme spinx-theme/marp-theme.css --allow-local-files --pptx --html false
 else
-    marp docs/intro.md --theme spinx-theme/marp-theme.css --allow-local-files --pptx
+    marp docs/intro.md --theme spinx-theme/marp-theme.css --allow-local-files --pptx --html false
 fi
 
