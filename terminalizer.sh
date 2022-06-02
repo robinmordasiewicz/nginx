0ae02409df8783f1a0e70330450e214941abfd74
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-06-02 17:38:54 -0400

    updating intro

--- terminalizer.sh
+++ terminalizer.sh
@@ -6,6 +6,4 @@ then
     STEP=1
 fi
 
-xvfb-run -s "-screen 0 1920x1080x16" \
-    terminalizer render -s $STEP recording -o tmp.gif \
-    && gifsicle --colors 256 --use-col=web -O3 < tmp.gif > rendered.gif
+xvfb-run -s "-screen 0 1920x1080x16" terminalizer render -s $STEP recording -o tmp.gif && gifsicle --colors 256 --use-col=web -O3 < tmp.gif > rendered.gif
